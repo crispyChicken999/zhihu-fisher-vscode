671653fb1de3f770b1d62d8cdd4e18389f31f2e0
crispyChicken999 <1483091515@qq.com>
2025-11-24 09:35:46 +0800

    fix: 移除回答排序选择组件中的多余空格

--- src/core/zhihu/webview/components/answer-sort.ts
+++ src/core/zhihu/webview/components/answer-sort.ts
@@ -40,7 +40,7 @@ export class AnswerSortComponent implements Component {
     const checkIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" viewBox="0 0 24 24"><path fill="currentColor" d="M9 16.17L4.83 12l-1.42 1.41L9 19L21 7l-1.41-1.41z"/></svg>`;
 
     return `
-      <div class="answer-sort-selector" >
+      <div class="answer-sort-selector">
         <div class="answer-sort-button" onclick="toggleAnswerSortPopover()">
           ${sortIcon}
         </div>
